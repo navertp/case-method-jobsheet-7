172dc16959a805f748eb8e776369ef2d94016abb
BagusWichaksono <baguswichaksonoamanulloh@gmail.com>
2025-04-17 08:56:33 +0700

    Bagus : MataKuliah

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,3 +1,17 @@
 public class MataKuliah {
-    
+    String kodeMK;
+    String namaMK;
+    int sks;
+
+    public MataKuliah(String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+
+    public void tampilMataKuliah() {
+        System.out.println("Kode Mata Kuliah : " + kodeMK);
+        System.out.println("Nama Mata Kuliah : " + namaMK);
+        System.out.println("SKS : " + sks);
+    }
 }
